0068b51b11f36b38a7d70cc2323c05ad99ee38e4
vaart <doigrayu@gmail.com>
2016-05-23 15:21:18 -0400

    added clocks

---
+++ js/clocks.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+		var dt 				= new Date();
+		var hh 				= dt.getHours();
+		var mm 				= dt.getMinutes();
+		var ss 				= dt.getSeconds();
+		function clock(){
+			setTimeout(function(){
+				dt=new Date();
+				hh=dt.getHours();
+				mm=dt.getMinutes();
+				ss=dt.getSeconds();
+
+				if(ss<10 ){
+					ss="0"+ss;
+				}
+				if(mm<10){
+					mm="0"+mm;
+				}
+				if(hh<10){
+					hh="0"+hh;
+				}
+
+				if( hh<18 ){
+					$("body").css("background", "orange");
+				}else{
+					$("body").css("background", "black");
+				}
+					$("#ss").html( ss );
+					$("#mm").html( mm );
+					$("#hh").html( hh );
+
+
+					
+					clock();
+			}, 1000);
+		}
+		clock();
+});
